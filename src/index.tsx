2e4d39fa28fee6f71dc957d3ccfa6ed0418350e9
eddie32 <liu599>
2024-09-08 17:30:20 +0800

    Finish framework

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+    const root = ReactDOM.createRoot(rootEl);
+    root.render(
+        <React.StrictMode>
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <App/>
+            </DevSupport>
+        </React.StrictMode>,
+    );
 }
