f359c78d26dbd6a19b316778a01cf17478f15dab
liu599 <970228409@qq.com>
2024-09-13 23:32:03 +0800

    api update

--- src/pages/TaggingSetting/api.ts
+++ src/pages/TaggingSetting/api.ts
@@ -6,8 +6,67 @@ export const customAxios = new CustomAxios({
 })
 export const customRequest = customAxios.instance
 
-export async function getTaggingSetting(data: any){
+export async function getTaggingSetting(
+    filter: object = {},
+    pageNumber: number,
+    pageSize: number,
+){
   return await customRequest.post('/napi-prod/llm/source_search', {
-    ...data,
+    sourceType: 'tagging',
+    filter,
+    pager: {
+      pageNumber,
+      pageSize
+    }
   })
 }
+
+export async function getCasesBySuite(
+    suiteId: number,
+    pageNumber: number,
+    pageSize: number,
+){
+  return await customRequest.post('/napi-prod/llm/source_search', {
+    sourceType: 'caseBySuiteId',
+    filter: {
+      suiteId,
+    },
+    pager: {
+      pageNumber,
+      pageSize
+    }
+  })
+}
+
+export async function getSuite(
+    suiteId: number
+){
+  return await customRequest.post('/napi-prod/llm/source_search', {
+    sourceType: 'suite',
+    filter: {
+      suiteId,
+    },
+    pager: {
+      pageNumber: 1,
+      pageSize: 1
+    }
+  })
+}
+
+export async function getTag(
+    filter: object = {},
+    pageNumber: number,
+    pageSize: number,
+) {
+  return await customRequest.post('/napi-prod/llm/source_search', {
+    sourceType: 'tag',
+    filter,
+    pager: {
+      pageNumber,
+      pageSize
+    }
+  })
+}
+
+
+
--- src/pages/TaggingSetting/index.tsx
+++ src/pages/TaggingSetting/index.tsx
@@ -3,14 +3,7 @@ import {getTaggingSetting} from "@/pages/TaggingSetting/api.ts";
 
 export default () => {
   useEffect(() => {
-    getTaggingSetting({
-      sourceType: 'tagging',
-      filter: {},
-      pager: {
-        pageNumber: 1,
-        pageSize: 20
-      }
-    })
+    getTaggingSetting({}, 1, 20)
   }, []);
   return (
     <div>Tagging Setting</div>
